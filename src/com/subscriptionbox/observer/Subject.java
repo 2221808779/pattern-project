fa45db9bba36838dadfe1e78a489e923669890cf
hiba soltan <hiba@22.com>
2026-05-13 19:26:51 +0200

    Remove builder, strategy, observer and add rest of project

--- src/com/subscriptionbox/observer/Subject.java
+++
@@ -1,9 +0,0 @@
-package com.subscriptionbox.observer;
-
-import com.subscriptionbox.model.Order.OrderStatus;
-
-public interface Subject {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers(String orderId, OrderStatus status, String message);
-}
